163b9b1e9a77123ed1f558a4eb8a0d2347e88cb9
evans <karlinevans333@gmail.com>
2019-10-04 14:04:44 +0700

    authentication

--- main.js
+++ main.js
@@ -303,7 +303,20 @@ function signOut() {
 
 function cekLogin(){
     if (localStorage.getItem('token')){
-        return true
+        $.ajax({
+            url: 'http://localhost:3000/user/authentication',
+            method: 'get',
+            headers: {
+                token : localStorage.getItem('token')
+            }
+        })
+            .done(status =>{
+                return true
+            })
+            .fail(err => {
+                $(`#modalForm`).modal("show")
+                return false
+            })
     }
     else{
         $(`#modalForm`).modal("show")
